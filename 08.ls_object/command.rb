8ce7ce1fa4a2f929af94aa1a7423ddaed83f025e
Saki-htr <saki.hattori0116@gmail.com>
2021-11-11 16:49:21 +0900

    出力に必要な情報を取得するためのFileInfoクラスを作成

--- 08.ls_object/command.rb
+++ 08.ls_object/command.rb
@@ -2,15 +2,20 @@
 
 require 'optparse'
 require_relative 'file_list'
+require_relative 'file_info'
 
 class LsCommand
   def main
     options = ARGV.getopts('arl')
     options
     files = FileList.new(options['a'],options['r']).decide_files
-    files
-
+    
   #file_listをインスタンス作成に使って、lオプションに必要な情報を取得するクラスを作る
+  file_infos = files.map do |file|
+    FileInfo.new(file)
+  end
+  p file_info
+
   #lオプションならLongFormatter呼び出し、それ以外はShortFormatter呼び出す
   end
 end
---
+++ 08.ls_object/file_info.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class FileInfo
+  def initialize(file)
+    @file = file
+  end
+end
