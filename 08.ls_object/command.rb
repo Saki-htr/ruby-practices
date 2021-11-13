0c9a19ec6be3f1b8051ea2ab91c04861bab06011
Saki-htr <saki.hattori0116@gmail.com>
2021-11-13 23:25:21 +0900

    各クラスを使う側のコードを実装

---
+++ 08.ls_object/command.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require_relative 'file_list'
+require_relative 'file_info'
+require_relative 'long_formatter'
+require_relative 'short_formatter'
+
+class LsCommand
+  def main
+    options = ARGV.getopts('arl')
+    files = FileList.new(options['a'], options['r']).decide_files
+    file_infos = files.map { |file| FileInfo.new(file) }
+    options['l'] ? LongFormatter.new(file_infos).output : ShortFormatter.new(file_infos).output
+  end
+end
+
+LsCommand.new.main
